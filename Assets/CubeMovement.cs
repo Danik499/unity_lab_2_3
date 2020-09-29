5a830a39747f31b578a156b0b8e526596bcd81a7
Danik499 <danikshcherban499@gmail.com>
2020-09-29 10:25:38 +0300

    lab_2_3

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+    
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (transform.position.x <= -6)
+        {
+            direction *= -1;
+        }
+        if (transform.position.x >= 6)
+        {
+            direction *= -1;
+        }
+    }
+
+   
+}
